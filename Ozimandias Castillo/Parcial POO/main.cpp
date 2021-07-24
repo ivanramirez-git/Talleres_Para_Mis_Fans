231024490a6cb8b99020ff123c9bfa612f9551cb
RenixExtreme <ivanrene10@gmail.com>
2021-07-24 18:20:56 -0500

    Punto4 listo

--- Ozimandias Castillo/Parcial POO/main.cpp
+++ Ozimandias Castillo/Parcial POO/main.cpp
@@ -14,5 +14,7 @@ int main() {
     enemigo.setEspecialidadAdd("BARCO");
     enemigo.setEspecialidadAdd("SUBMARINO");
 
+    
+
     return 0;
 }
--- Rene Andres/Punto4.cpp
+++ Rene Andres/Punto4.cpp
@@ -6,51 +6,30 @@ using namespace std;
 
 int obtenerCubo(int n){
     int num=0, numeros=-1;
-    bool fin=true;
     
-    
-    
-    
-    /*    
-    for(int i=-1;i<n;i+=2){
-        numeros=0;
-        cout<<"."<<endl;
-        for(int j=-1;j<i;j+=2){
-            numeros+=numeros+2;
-        }
-    }
-    cout<<numeros<<endl;
-
-
-
-    for (int i = 1; i <= n; i++)
-    {
-        if((i%2)!=0){
-            cout<<i<<endl;
-            for(int j=1;j<=i;j++){
-                if(j==i){
-                    num++;
-                }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            
+            numeros+=2;
+            if(i==n){
+                num=num+numeros;
             }
         }
-        if(num==n){
-            return num;
-        }
     }
-    */
-    return numeros;
+
+        return num;
 }
 
 int main(){
 
     srand(3);
     
-    obtenerCubo(5);
+    //obtenerCubo(5);
     
-    /*for(int i = 0; i<10; i++){
+    for(int i = 0; i<10; i++){
         int n=rand() % 10 + 1; // Aleatorio en el intervalo [1,10]   
-        //cout<<"El cubo de "<<n<<" es: "<<obtenerCubo(n)<<endl;
-    }*/
+        cout<<"El cubo de "<<n<<" es: "<<obtenerCubo(n)<<endl;
+    }
     //cout << "Hello World!";
     return 0;
 }
